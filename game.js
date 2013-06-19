4496e01fa80ad058152a042a85f4e9b306fc5635
App Academy Student <student@appacademy.io>
2013-06-19 08:52:37 -0700

    Line lengths > 80 characters are ugly.

--- game.js
+++ game.js
@@ -2,10 +2,13 @@ $(function() {
 	var wx = $(window).width();
 	var wy = $(window).height();
 
-	var canvas = $("<canvas id='awesome_canvas' width='" + wx + "' height='" + wy + "'></canvas>");
+	var canvas = $(
+		"<canvas id='canvas' width='" + wx + "' height='" + wy + "'></canvas>"
+	);
+
 	$('body').append(canvas);
 
-	var canvas = document.getElementById('awesome_canvas');
+	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext("2d");
 
 	new Asteroids.Game(ctx).start();
